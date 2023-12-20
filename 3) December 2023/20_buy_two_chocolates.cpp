c21aeeb4341a80c44a653f744aa4bc9bd0943a46
Pratham Gupta <prathamgupta.pg1@gmail.com>
2023-12-20 20:03:09 +0530

    Day 81: Buy Two Chocolates

---
+++ 3) December 2023/20_buy_two_chocolates.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int buyChoco(vector<int> &prices, int money)
+    {
+        int minimum = min(prices[0], prices[1]);
+        int secondMinimum = max(prices[0], prices[1]);
+
+        for (int i = 2; i < prices.size(); i++)
+        {
+            if (prices[i] < minimum)
+            {
+                secondMinimum = minimum;
+                minimum = prices[i];
+            }
+            else if (prices[i] < secondMinimum)
+            {
+                secondMinimum = prices[i];
+            }
+        }
+
+        int minCost = minimum + secondMinimum;
+
+        if (minCost <= money)
+            return money - minCost;
+
+        return money;
+    }
+};
