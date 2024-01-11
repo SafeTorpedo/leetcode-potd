27c850c0d7f464d6354d55d809e73f58718c52bb
Pratham Gupta <prathamgupta.pg1@gmail.com>
2024-01-11 10:31:12 +0530

    Day 103: Maximum Difference Between Node and Ancestor

---
+++ 4) January 2024/11_maximum_difference_between_node_and_ancestor.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    int maxAncestorDiff(TreeNode *root)
+    {
+        int ans = 0;
+        dfs(root, ans);
+        return ans;
+    }
+
+private:
+    pair<int, int> dfs(TreeNode *root, int &ans)
+    {
+        if (root == NULL)
+            return {INT_MAX, INT_MIN};
+
+        auto left = dfs(root->left, ans);
+        auto right = dfs(root->right, ans);
+
+        int minVal = min(root->val, min(left.first, right.first));
+        int maxVal = max(root->val, max(left.second, right.second));
+
+        ans = max({ans, abs(minVal - root->val), abs(maxVal - root->val)});
+
+        return {minVal, maxVal};
+    }
+};
