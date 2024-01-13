daa1aa3d7eee276f59c7514003cc22546678ca47
Pratham Gupta <prathamgupta.pg1@gmail.com>
2024-01-13 15:49:28 +0530

    Day 105: Minimum Steps to Make Two Strings Anagram

---
+++ 4) January 2024/13_minimum_steps_to_make_two_strings_anagram.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int minSteps(string s, string t)
+    {
+        vector<int> count(26, 0);
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            count[t[i] - 'a']++;
+            count[s[i] - 'a']--;
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < 26; i++)
+            ans += max(0, count[i]);
+
+        return ans;
+    }
+};
