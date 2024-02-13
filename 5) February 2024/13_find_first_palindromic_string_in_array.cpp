af8f3b3b39824b4303b8e80999c7405809930902
Pratham Gupta <prathamgupta.pg1@gmail.com>
2024-02-13 23:43:35 +0530

    Day 136: Find First Palindromic String in the Array

---
+++ 5) February 2024/13_find_first_palindromic_string_in_array.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    string firstPalindrome(vector<string> &words)
+    {
+        for (string s : words)
+        {
+            if (isPalindrome(s))
+            {
+                return s;
+            }
+        }
+
+        return "";
+    }
+
+private:
+    bool isPalindrome(string &s)
+    {
+        int start = 0;
+        int end = s.size() - 1;
+
+        while (start <= end)
+        {
+            if (s[start] != s[end])
+            {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+};
