9f2feaa4ec0fbd51f45889c4bddadc4de2ee8156
Pratham Gupta <prathamgupta.pg1@gmail.com>
2024-02-19 13:36:16 +0530

    Day 142: Power of Two

---
+++ 5) February 2024/19_power_of_two.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isPowerOfTwo(int n)
+    {
+        if (n <= 0)
+            return false;
+
+        if ((n & (n - 1)) == 0)
+            return true;
+
+        return false;
+    }
+};
