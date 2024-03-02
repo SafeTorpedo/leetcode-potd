f7ca8fa2a9e98f58aff18699392f49b3d0d17daa
Pratham Gupta <prathamgupta.pg1@gmail.com>
2024-03-02 12:15:43 +0530

    Day 154: Squares of Sorted Array

---
+++ 6) March 2024/2_squares_of_sorted_array.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> sortedSquares(vector<int> &nums)
+    {
+        int i = 0, j = nums.size() - 1, k = j;
+        vector<int> ans(k + 1, 0);
+        while (k >= 0)
+        {
+            if (abs(nums[i]) > abs(nums[j]))
+                ans[k--] = nums[i] * nums[i++];
+            else
+                ans[k--] = nums[j] * nums[j--];
+        }
+        return ans;
+    }
+};
