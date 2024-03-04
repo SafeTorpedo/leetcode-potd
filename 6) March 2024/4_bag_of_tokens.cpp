7eb3ce95f565184d7985dca2c83a1b37cb2221aa
Pratham Gupta <prathamgupta.pg1@gmail.com>
2024-03-04 11:54:50 +0530

    Day 156: Bag of Tokens

---
+++ 6) March 2024/4_bag_of_tokens.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int bagOfTokensScore(vector<int> &tokens, int power)
+    {
+        int score = 0;
+        sort(tokens.begin(), tokens.end());
+        deque<int> deque(tokens.begin(), tokens.end());
+
+        while (!deque.empty())
+        {
+            if (power >= deque.front())
+            {
+                power -= deque.front();
+                deque.pop_front();
+                score += 1;
+            }
+            else if (deque.size() > 1 && score > 0)
+            {
+                power += deque.back();
+                deque.pop_back();
+                score -= 1;
+            }
+            else
+            {
+                return score;
+            }
+        }
+        return score;
+    }
+};
