259fbb805f4028e7aba85f56ac538455f2d39751
Pratham Gupta <prathamgupta.pg1@gmail.com>
2024-04-05 09:18:48 +0530

    Day 188: Make the String Great

---
+++ 7) April 2024/5_make_the_string_great.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    string makeGood(string s)
+    {
+        string ans;
+
+        for (char ch : s)
+        {
+            if (ans.size() > 0 && (ans.back() + 32 == ch || ans.back() - 32 == ch))
+                ans.pop_back();
+            else
+                ans.push_back(ch);
+        }
+
+        return ans;
+    }
+};
