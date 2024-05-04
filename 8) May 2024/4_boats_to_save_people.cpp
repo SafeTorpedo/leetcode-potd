0105d8584995506930f4de243fabcf9c9b560fd5
Pratham Gupta <prathamgupta.pg1@gmail.com>
2024-05-04 13:41:14 +0530

    Day 217: Boats to Save People

---
+++ 8) May 2024/4_boats_to_save_people.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int numRescueBoats(vector<int> &people, int limit)
+    {
+        int n = people.size();
+        sort(people.begin(), people.end());
+
+        int i = 0, j = n - 1;
+        int ans = 0;
+
+        while (i <= j)
+        {
+            if (people[j] + people[i] <= limit)
+            {
+                i++;
+                j--;
+            }
+            else
+                j--;
+
+            ans++;
+        }
+
+        return ans;
+    }
+};
