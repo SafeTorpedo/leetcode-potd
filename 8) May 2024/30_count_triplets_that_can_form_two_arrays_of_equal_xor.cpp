5f70d614404706ffdd7b1bdbb13d2351318c57a5
Pratham Gupta <prathamgupta.pg1@gmail.com>
2024-05-30 10:27:24 +0530

    Day 243: Count Triplets that can form two Arrays of equal XOR

---
+++ 8) May 2024/30_count_triplets_that_can_form_two_arrays_of_equal_xor.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int countTriplets(vector<int> &arr)
+    {
+        int size = arr.size();
+        int count = 0;
+        int prefix = 0;
+
+        unordered_map<int, int> countMap = {{0, 1}}, totalMap;
+
+        for (int i = 0; i < size; ++i)
+        {
+            prefix ^= arr[i];
+
+            count += countMap[prefix]++ * i - totalMap[prefix];
+
+            totalMap[prefix] += i + 1;
+        }
+
+        return count;
+    }
+};
