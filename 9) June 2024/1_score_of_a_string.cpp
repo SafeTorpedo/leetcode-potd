4d9491b5b683cdd430e828c420cd5c47b43a48f2
Pratham Gupta <prathamgupta.pg1@gmail.com>
2024-06-01 12:14:42 +0530

    Day 245: Score of a String

---
+++ 9) June 2024/1_score_of_a_string.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int scoreOfString(string s)
+    {
+        int score = 0;
+        for (int i = 0; i < s.size() - 1; ++i)
+        {
+            score += abs(s[i] - s[i + 1]);
+        }
+        return score;
+    }
+};
