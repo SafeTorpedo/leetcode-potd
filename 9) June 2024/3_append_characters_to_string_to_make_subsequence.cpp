487a9f7ab1c57fb345b080300887dcf295e2628e
Pratham Gupta <prathamgupta.pg1@gmail.com>
2024-06-03 19:44:16 +0530

    Day 247: Append Characters to String to make subsequence

---
+++ 9) June 2024/3_append_characters_to_string_to_make_subsequence.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int appendCharacters(string s, string t)
+    {
+        int first = 0, longestPrefix = 0;
+
+        while (first < s.length() && longestPrefix < t.length())
+        {
+            if (s[first] == t[longestPrefix])
+            {
+                longestPrefix++;
+            }
+            first++;
+        }
+
+        return t.length() - longestPrefix;
+    }
+};
