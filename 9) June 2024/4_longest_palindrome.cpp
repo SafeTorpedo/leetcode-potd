0accc6aa6034ad22c2e7a111d2528aa58ee8ff9e
Pratham Gupta <prathamgupta.pg1@gmail.com>
2024-06-04 15:41:48 +0530

    Day 248: Longest Palindrome

---
+++ 9) June 2024/4_longest_palindrome.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int longestPalindrome(string s)
+    {
+        unordered_map<char, int> mp;
+
+        for (char &ch : s)
+        {
+            mp[ch]++;
+        }
+
+        int ans = 0, oddFreq = 0;
+        bool odd = false;
+
+        for (auto &it : mp)
+        {
+            if (it.second % 2 == 0)
+                ans += it.second;
+            else
+            {
+                ans += it.second - 1;
+                odd = true;
+            }
+        }
+
+        if (odd)
+            ans++;
+
+        return ans;
+    }
+};
